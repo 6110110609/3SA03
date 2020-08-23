6f333a6f7cb019845744e546d23693392c6ca19d
6110110609 <6110110609@psu.ac.th>
2020-08-23 13:23:09 +0700

    part7-5

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -4,18 +4,30 @@ import WordCard from './WordCard';
 import AppHeader from './AppHeader';
 
 let word = ''
+let text = ''
 
 let rand = Math.floor(Math.random() * 3);
 
 switch (rand) {
     case 0:
         word = 'Hello'
+        text = 'คำพูดกล่าวทักทาย'
         break;
     case 1:
         word = 'World'
+        text = 'ที่อยู่อาศัยของมนุษย์ทุกคน'
         break;
     case 2:
         word = 'React'
+        text = 'แอปที่ใช้ทำผลงานชิ้นนี้'
+        break;
+    case 3:
+        word = 'Suthon'
+        text = 'ชื่ออาจารย์แลปนี้'
+        break;
+    case 4:
+        word = 'Internet'
+        text = 'เครื่องข่ายการสื่อสารที่ทุกคนใช้ติดต่อกันบนโลกออนไลน์'
         break;
 
     default:
@@ -27,12 +39,15 @@ function App() {
         <div>
             <AppHeader />
             <div className='cardstart'>
-                <WordCard value = {word} />
+                <WordCard value={word} />
             </div>
             <div>
                 <center>
                     <button className='button'>
                         <a href="http://localhost:3000/" >RESET</a>
+                    </button>
+                    <button className={'button'} onClick={() => hintans()}>
+                        <a>HINT</a>
                     </button>
                     <button className={'button'} onClick={() => showans(word)}>
                         <a>ANSWER</a>
@@ -46,6 +61,10 @@ function App() {
 
 function showans(word) {
     alert("The answer is " + word)
+}
+
+function hintans() {
+    alert("Hint is " + text)
 }
 
 export default App;
